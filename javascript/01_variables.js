0a17dad4b7428582d2de87bb848f8cb86a1ee29b
Divya Gautam <91388222+Divya-gautam@users.noreply.github.com>
2023-10-02 10:35:24 +0000

    datatypes

---
+++ javascript/01_variables.js
@@ -0,0 +1,5 @@
+const accountId= 12335
+let accountEmail= "abs@gmail.com"
+var accountNumber= "2345"
+let accountState;
+console.table([accountId,accountEmail,accountNumber,accountState]);
---
+++ javascript/02_datatypes.js
@@ -0,0 +1,8 @@
+"use strict"; //use for apply new version of javascript 
+
+// alert(3+3)  // it will not give an output because we r in node js not in browser
+ let name= "Divya"
+ let age = 21
+ let IsLoggedIn=true
+  console.log(typeof IsLoggedIn);
+  console.log(typeof null);
