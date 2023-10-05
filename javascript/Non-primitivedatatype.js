4025c3558e55fca8179a2fef301d5a857084d0c6
Divya Gautam <91388222+Divya-gautam@users.noreply.github.com>
2023-10-05 11:38:41 +0000

    Non primitive datatype

---
+++ javascript/Non-primitivedatatype.js
@@ -0,0 +1,10 @@
+const one =["tiger" , "dog","cat"];   //array
+let two = {                           // object
+    username :"divya",
+    age:21,
+    number:9887277277
+}
+function myfun(){                     //function 
+    console.log("hello world!")
+}
+console.log(typeof one);
